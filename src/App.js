ee4dc01a3c16538e7ee09e1efa27ad2192075e31
apelpapa <apelpapa@gmail.com>
2024-08-26 01:45:22 -0600

    1

--- src/App.js
+++ src/App.js
@@ -1,27 +1,24 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
-import AboutUs from './pages/About';
-import Puppies from './pages/Puppies';
-import Contact from './pages/Contact';
-import Home from './pages/Home';
 import Footer from './components/Footer';
+import Home from './pages/Home';
+import Puppies from './pages/Puppies';
+import AboutUs from './pages/About';
+import Contact from './pages/Contact';
 
 function App() {
   return (
     <Router>
-      <div className="app-container"> {/* Added container for layout */}
-        <Navbar />
-        <main> {/* Main content area */}
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about-us" element={<AboutUs />} />
-            <Route path="/available-puppies" element={<Puppies />} />
-            <Route path="/contact" element={<Contact />} />
-          </Routes>
-        </main>
-        <Footer />
-      </div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/about-us" element={<AboutUs />} />
+        <Route path="/available-puppies" element={<Puppies />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+      <Footer />
     </Router>
   );
 }
